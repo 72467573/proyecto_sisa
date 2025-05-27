726418a19e360bffce46c4b79e30fc6cad65f6d5
GiusseppeALor <112878090+GiusseppeALor@users.noreply.github.com>
2025-05-27 12:52:33 -0500

    back agrgado

---
+++ js/auth.js
@@ -0,0 +1,55 @@
+// REGISTRO DE USUARIO
+document.getElementById('formRegistro').addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const nombre = document.getElementById('registroNombre').value;
+  const email = document.getElementById('registroEmail').value;
+  const contrasena = document.getElementById('registroContrasena').value;
+  const numero_telefono = document.getElementById('registroTelefono').value;
+
+  try {
+    const res = await fetch('http://localhost:3000/auth/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ nombre, email, contrasena, numero_telefono })
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      alert(data.message);
+      document.getElementById('formRegistro').reset();
+    } else {
+      alert(data.error || 'Error en el registro');
+    }
+  } catch (err) {
+    console.error('Error en el registro:', err);
+  }
+});
+
+// LOGIN DE USUARIO
+document.getElementById('formLogin').addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('loginEmail').value;
+  const contrasena = document.getElementById('loginContrasena').value;
+
+  try {
+    const res = await fetch('http://localhost:3000/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, contrasena })
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      alert(data.message);
+      window.location.href = '/Logeado/index.html'; 
+    } else {
+      alert(data.error || 'Error en el login');
+    }
+  } catch (err) {
+    console.error('Error en el login:', err);
+  }
+});
---
+++ js/form.js
@@ -0,0 +1,31 @@
+document.getElementById('formularioInfo').addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const data = {
+        nombre: document.getElementById('nombre_completo').value,
+        correo: document.getElementById('correo').value,     
+        telefono: document.getElementById('telefono').value,
+        consulta: document.getElementById('consulta').value   
+    };
+
+
+    try {
+        const response = await fetch('http://localhost:3000/formulario/enviar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            alert('Formulario enviado correctamente.');
+            document.getElementById('formularioInfo').reset();
+        } else {
+            alert('Error al enviar el formulario.');
+        }
+    } catch (error) {
+        console.error('Error al enviar:', error);
+        alert('Error de conexión con el servidor.');
+    }
+});
